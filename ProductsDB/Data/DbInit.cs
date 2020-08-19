2d8d427c00653edb6a3fcbfc0775f603707ff301
oc777 <oc222ba@student.lnu.se>
2020-08-19 19:55:10 +0200

    parsing csv data

--- ProductsDB/Data/DbInit.cs
+++ ProductsDB/Data/DbInit.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using CsvHelper;
+using ProductsDB.Models;
 
 namespace ProductsDB.Data
 {
@@ -17,10 +22,38 @@ namespace ProductsDB.Data
             // seed db if empty
             if (context.Markets.Any())
             {
+                // DB has been seeded
                 return;
             }
 
             Console.WriteLine("tada");
+
+            var CsvPath = Path.Combine("Data", "SeedData", "price_detail.csv");
+            List<PriceDetail> prices = new List<PriceDetail>();
+            List<Product> products = new List<Product>();
+            List<Market> markets = new List<Market>();
+
+            // parse file using CSVHelper
+            using (var stream = File.OpenRead(CsvPath))
+            using (var reader = new StreamReader(stream))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.Delimiter = "\t";
+                csv.Configuration.TypeConverterOptionsCache.GetOptions<DateTime?>().NullValues.Add("NULL");
+
+                prices = csv.GetRecords<PriceDetail>().ToList();
+
+                // go to the begining of stream
+                stream.Position = 0;
+                products = csv.GetRecords<Product>().ToList();
+
+                stream.Position = 0;
+                markets = csv.GetRecords<Market>().ToList();
+            }
+
+            Console.WriteLine(prices.Count);
+            Console.WriteLine(products.Count);
+            Console.WriteLine(markets.Count);
         }
 
     }
