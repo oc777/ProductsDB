80f7afe01a06515baa39a695010afe9482830592
oc777 <oc222ba@student.lnu.se>
2020-08-19 16:10:02 +0200

    adding db context

---
+++ ProductsDB/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ProductsDB.Models;
+
+namespace ProductsDB.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public DbSet<Market> Markets { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<PriceDetail> PriceDetails { get; set; }
+    }
+}
