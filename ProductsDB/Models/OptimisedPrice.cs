19736ed99b99606c6678baac65bd6fadb56ff8ff
oc777 <oc222ba@student.lnu.se>
2020-08-24 20:28:05 +0200

    adding model for optimised prices

---
+++ ProductsDB/Models/OptimisedPrice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProductsDB.Models
+{
+    public class OptimisedPrice
+    {
+        public string CatalogEntryCode { get; set; }
+        public string MarketId { get; set; }
+        public string CurrencyCode { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime? ValidUntil { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
