35dc18c62ad99944d662108956b8b9f2923c859e
oc777 <oc222ba@student.lnu.se>
2020-08-26 09:22:47 +0200

    view refactoring formatting date and price in Model instead of Page

--- ProductsDB/Models/OptimisedPrice.cs
+++ ProductsDB/Models/OptimisedPrice.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProductsDB.Models
 {
@@ -7,8 +8,11 @@ namespace ProductsDB.Models
         public string CatalogEntryCode { get; set; }
         public string MarketId { get; set; }
         public string CurrencyCode { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}")]
         public DateTime ValidFrom { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}")]
         public DateTime? ValidUntil { get; set; }
+        [DisplayFormat(DataFormatString = "{0:#.00}")]
         public decimal UnitPrice { get; set; }
     }
 }
